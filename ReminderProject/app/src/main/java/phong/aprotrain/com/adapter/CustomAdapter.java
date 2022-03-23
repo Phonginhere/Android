06ddaba037d6d2200184df0c78b8d7f1e4f60657
Phonginhere <tranhaiphong2016fpt@gmail.com>
2022-03-23 16:55:16 +0700

    80%

--- ReminderProject/app/src/main/java/phong/aprotrain/com/adapter/CustomAdapter.java
+++ ReminderProject/app/src/main/java/phong/aprotrain/com/adapter/CustomAdapter.java
@@ -1,25 +1,36 @@
 package phong.aprotrain.com.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import phong.aprotrain.com.CreateActivity;
+import phong.aprotrain.com.EditActivity;
+import phong.aprotrain.com.MainActivity;
 import phong.aprotrain.com.R;
 import phong.aprotrain.com.model.Reminder;
 
 public class CustomAdapter extends ArrayAdapter<Reminder> {
     private ArrayList<Reminder> reminders;
     private Context context;
+    private Button button;
 
     public CustomAdapter(Context context, ArrayList<Reminder> reminders) {
         super(context, 0, reminders);
@@ -34,7 +45,7 @@ public class CustomAdapter extends ArrayAdapter<Reminder> {
             view = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
 //        }
         TextView textNoiDung = (TextView) view.findViewById(R.id.textNoiDung);
-        Button button = (Button) view.findViewById(R.id.button);
+        button = (Button) view.findViewById(R.id.btnOption);
 
         textNoiDung.setText(selectedReminder.getNoidung());
         if(selectedReminder.isQuantrong() == true){
@@ -42,6 +53,58 @@ public class CustomAdapter extends ArrayAdapter<Reminder> {
         }else{
             button.setBackgroundColor(Color.rgb(255, 0, 0));
         }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initializing the popup menu and giving the reference as current context
+                PopupMenu popupMenu = new PopupMenu(getContext(), button);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(getContext(), "You Clicked " + position, Toast.LENGTH_SHORT).show();
+                        menuItemClicked(menuItem, position);
+
+                        return true;
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
+            }
+        });
+
+
+
         return view;
     }
+
+    // When user click on Menu Item.
+    // @return true if event was handled.
+    private boolean menuItemClicked(MenuItem item, int position) {
+        switch (item.getItemId()) {
+            case R.id.optionEdit:
+                Toast.makeText(getContext(), "Bookmark", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(),EditActivity.class);
+
+                getContext().startActivity(intent);
+                MainActivity.onEditViewReminder(position);
+                break;
+            case R.id.deleteReminder:
+                Toast.makeText(getContext(), "Upload", Toast.LENGTH_SHORT).show();
+                MainActivity.onRemoveReminder(position);
+                Intent intentRemove = new Intent(getContext(),MainActivity.class);
+                getContext().startActivity(intentRemove);
+                break;
+            default:
+                Toast.makeText(getContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+
 }
